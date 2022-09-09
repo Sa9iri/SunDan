0c1379a14608e9177b65ad45774af583f8e93043
紫陌 <76895152+ZiMoFangCong@users.noreply.github.com>
2022-09-09 13:00:02 +0800

    Add files via upload

---
+++ js/ui.js
@@ -0,0 +1,59 @@
+var show = true;
+var isMobile = 'ontouchstart' in document.documentElement;
+function showTextarea(id) {
+    let inputDiv = document.getElementById(id);
+    if (show == false) {
+        inputDiv.classList.remove('input-active')
+        inputDiv.classList.add('input-inactive')
+        show = true;
+    } else {
+        inputDiv.classList.remove('input-inactive')
+        inputDiv.classList.add('input-active')
+        show = false;
+    }
+}
+
+// // 检测回车事件
+// function clickEnter(e) {
+//     var theEvent = e || window.event;
+//     var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+//     if (code == 13) {
+//         // 阻止事件的默认行为
+//         e.preventDefault();
+//         startMusic();
+//     }
+// }
+
+for (let node of document.querySelectorAll('.triangle')) {
+    let key = node.querySelector('.key').id;
+    if (isMobile) {
+        node.addEventListener('touchstart', e => {
+            if (show != -1) play(key, true);
+        });
+    } else {
+        node.addEventListener('mousedown', e => {
+            if (show != -1) play(key, true);
+        });
+    }
+    removeID(node.parentNode.childNodes[1]);
+}
+
+let bpmField = document.getElementById("bpm");
+bpmField.addEventListener('focusout', () => {
+    updateBpm(bpmField.value);
+});
+
+document.getElementById('transpose-num').addEventListener('change', e => {
+    transpose= parseInt(e.target.value);
+});
+
+function removeID(spread) {
+    spread.addEventListener("animationend", function () {
+        spread.removeAttribute('id');
+    }, false);
+}
+
+function selectMode(node){
+    mode = node.innerText;
+    document.querySelector("#modeText").innerText = node.innerText;
+}
